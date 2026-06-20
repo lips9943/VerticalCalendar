fc209ebdab0307ec782f4b498a0a79c1c14abc38
lips9943 <lips9943@gmail.com>
2026-06-20 23:41:21 +0900

    feat: add VCDefaultMonthSelectionCell to TestPreview

---
+++ TestPreview/TestPreview/VCDefaultMonthSelectionCell.swift
@@ -0,0 +1,12 @@
+//
+//  VCDefaultMonthSelectionCell.swift
+//  TestPreview
+//
+//  Created by 고혁준 on 6/20/26.
+//
+
+import VerticalCalendar
+
+final class VCDefaultMonthSelectionCell: VCMonthSelectionViewController.MonthSelectionCell {
+    
+}
