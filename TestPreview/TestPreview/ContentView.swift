e48f84727507bdb2c91d87a96fd71004548039bb
lips9943 <lips9943@gmail.com>
2026-06-21 23:47:40 +0900

    test: support presenting month selection via tab bar double-tap in preview

--- TestPreview/TestPreview/ContentView.swift
+++ TestPreview/TestPreview/ContentView.swift
@@ -11,22 +11,45 @@ import VerticalCalendar
 
 struct ContentView: View {
     @State var tabIndex: Int = 0
+    @State var isCalendar: Bool = true
+    @State var calendarDoubleTabbed: Bool = false
+    
     var body: some View {
-        TabView(selection: $tabIndex) {
+        let selection = Binding<Int>(
+            get: { self.tabIndex },
+            set: { index, t in
+                if index == 0 && isCalendar {
+                    calendarDoubleTabbed = true
+                    print("double")
+                    return
+                }
+                
+                isCalendar = index == 0 ? true : false
+                tabIndex = index
+                calendarDoubleTabbed = false
+            }
+        )
+        
+        TabView(selection: selection) {
             Tab(value: 0) {
-                VCTestPreview()
+                VCTestPreview(presentMonthSelectionVC: $calendarDoubleTabbed)
             } label: {
                 Text("Calendar")
             }
-
+            
             Tab(value: 1) {
                 Text("hi")
             } label: {
                 Text("더미")
             }
-
+            
+            
+            Tab(value: 2) {
+                Text("ds")
+            } label: {
+                Text("dasdf")
+            }
         }
-        
     }
 }
 
